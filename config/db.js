4ac9584ba72471e1df453edfa4dfe5ff571981a9
Isaac Paucar <isaipaucarchuquispuma@gmail.com>
2026-04-23 09:19:31 -0500

    feat: agregar configuración de conexión y script inicial de base de datos

--- config/db.js
+++ config/db.js
@@ -1,2 +1,30 @@
 const mysql = require('mysql2/promise') //Acceso
 requiere('dotenv').config() //Leer los valores del archivo de configuración
+
+//pool de conexiones => "conjunto de conexiones disponibles"
+//conexión "regular" (normal) => usuario1 => abre => proceso > cierra
+//pool "optimizado" => se crean todas las conexiones a ofrecer (10) => usuario1, usuario2, etc. usuario11( tamaño cola)
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_DB_NAME || "",
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 50,
+  timezone: "-05:00"
+});
+
+//Ejecutar la conexión - IIFE (Expresión de función invocada de forma inmediata)
+(async () => {
+  try{
+    const conn = await pool.getConnection(); //Conexion prestada
+    console.log("Conexión a MySQL correcta");
+    conn.release(); //devolucion
+  }catch (err){
+    console.error(err.message)
+  }
+})();
+
+module.exports = pool;
---
+++ database/database.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE tiendanode;
+USE tiendanode;
+
+CREATE TABLE IF NOT EXISTS marcas(
+	id 			INT AUTO_INCREMENT PRIMARY KEY,
+    nombremarca VARCHAR(100) NOT NULL,
+    create_at 	TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_at 	TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON
+    UPDATE CURRENT_TIMESTAMP
+)ENGINE = INNODB;
+
+/*
+INSERT INTO marcas (nombremarca) VALUES ('Samsumg');
+INSERT INTO marcas (nombremarca) VALUES ('AMD');
+UPDATE marcas SET nombremarca = 'HP' WHERE id = 1;
+SELECT * FROM marcas;
+*/
+
+-- Garantía en meses
+CREATE TABLE IF NOT EXISTS productos(
+	id 			INT AUTO_INCREMENT PRIMARY KEY,
+    idmarca		INT NOT NULL,
+    nombre		VARCHAR(150) NOT NULL,
+    precio		DECIMAL(7,2) NOT NULL,
+    garantia	TINYINT COMMENT 'Se deberá indicar en meses',
+    descripcion VARCHAR(100) NOT NULL,
+    fechacompra	DATE NOT NULL,
+    create_at 	TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_at 	TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT fk_idmarca_prd FOREIGN KEY (idmarca) REFERENCES marcas (id)
+)ENGINE = INNODB;
+
+INSERT INTO productos
+	(idmarca, nombre, precio, garantia, descripcion, fechacompra)
+    VALUES
+    (1, 'Monitor Led', 400, 24, 'Resolción Full HD - 60H', '2026-02-01'),
+    (2, 'Microprocesador', 700, 12, 'Ryzen 5 - 5200', '2026-03-10');
+    
+UPDATE productos SET descripcion = 'Resolucion FULL HD - 120Hz' WHERE id = 1;
+SELECT * FROM productos;
